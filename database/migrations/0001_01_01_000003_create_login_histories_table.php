e2c9947bf2354de4259978c09c631386c41f892f
Marsels <ipb22.m.strikaitis@vtdt.edu.lv>
2026-04-01 16:24:53 +0200

    Migration fix

--- database/migrations/0001_01_01_000003_create_login_histories_table.php
+++ database/migrations/0001_01_01_000003_create_login_histories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('login_Histories', function (Blueprint $table) {
+        Schema::create('login_histories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamp('login_time');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('login_Histories');
+        Schema::dropIfExists('login_histories');
     }
 };
